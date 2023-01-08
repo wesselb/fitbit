213ab2e926198bc22bd15e6dd4cda0b0270b2ac6
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-01-08 14:33:25 +0100

    Fix scraping allowance system

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,7 +1,8 @@
 import datetime
 from pathlib import Path
 
-from fitbit import authenticate, hr, hrv, br, spo2
+import pandas as pd
+from fitbit import authenticate, br, hr, hrv, spo2
 
 # Ensure that the output directories exist.
 out_dir = Path("output")
@@ -15,17 +16,28 @@ current = datetime.datetime.now() - datetime.timedelta(days=1)
 
 
 def fetch_and_dump(path, api_call, day):
+    """Fetch data and dump it to a CSV. Will only fetch data if it was not already
+    fetched.
+
+    Args:
+        path (str): Directory to dump the CSV into.
+        api_call (function): API function to call.
+        day (:class:`datetime.datetime`): Day to fetch data for.
+
+    Returns:
+        bool: `True` if any data was available, otherwise `False`.
+    """
     out = path / day.strftime("%Y-%m-%d.csv")
     if not out.exists():
         try:
             df = api_call(authenticate(), day)
             df.to_csv(str(out))
-            return True
         except Exception as e:
             print(f'Failed "{out}" with the following exception:', type(e), str(e))
             return False
     else:
-        return True
+        df = pd.read_csv(out)
+    return len(df) > 0
 
 
 # How many days without any data before stopping scraping?
@@ -42,5 +54,4 @@ while True:
         allowance -= 1
         if allowance == 0:
             break
-    else:
-        current -= datetime.timedelta(days=1)
+    current -= datetime.timedelta(days=1)
