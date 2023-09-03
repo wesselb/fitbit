4611e26c272770560dbc9a2aef1ce55b792f789e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2023-09-03 13:41:43 +0200

    Convert indices

--- scripts/scrape_do.py
+++ scripts/scrape_do.py
@@ -5,6 +5,11 @@ hrv = pd.read_parquet("output/hrv/hrv.parquet")
 spo2 = pd.read_parquet("output/spo2/spo2.parquet")
 br = pd.read_parquet("output/br/br.parquet")
 
+hr.index = pd.to_datetime(hr.index)
+hrv.index = pd.to_datetime(hrv.index)
+spo2.index = pd.to_datetime(spo2.index)
+br.index = pd.to_datetime(br.index)
+
 print(hr)
 print(hrv)
 print(spo2)
